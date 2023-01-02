c125731c71f603228a831f2788b2955ea2a205cb
lalimary <118160280+lalimary@users.noreply.github.com>
2023-01-02 12:17:09 +0530

    Add files via upload

---
+++ spa.js
@@ -0,0 +1,47 @@
+var app = angular.module('myApp', [], ['ui.bootstrap']);
+var app = angular.module('myApp', ['ngRoute']);
+app.config(function ($routeProvider) {
+    $routeProvider
+
+        .when('/', {
+            templateUrl: 'aboutus.html',
+            controller: 'FirstController'
+        })
+
+        .when('/manipulate', {
+            templateUrl: 'manipulate.html',
+            controller: 'SecondController'
+        })
+
+        .when('/mechanicdata', {
+            templateUrl: 'mechanicdata.html',
+            controller: 'ThirdController'
+        })
+
+        .otherwise({
+            redirectTo: '/'
+        });
+});
+
+
+app.controller('FirstController', function ($scope) {
+    $scope.message = 'We build a strong community for a better future';
+    $scope.message2 = 'Welcome to discussion cube';
+});
+
+
+app.controller('SecondController', function ($scope, $http) {
+    $http.get('https://raw.githubusercontent.com/lalimary/proj/main/sample.json')
+
+        .success(function (response) {
+            $scope.names = response.data;
+        });
+});
+
+app.controller('ThirdController', function ($scope, $http) {
+    $http.get('https://raw.githubusercontent.com/lalimary/proj/main/sample.json')
+        .success(function (response) {
+            $scope.names = response.data;
+            $scope.rowlimit = 10;
+        });
+});
